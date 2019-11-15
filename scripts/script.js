215eeb2e6223fefbe9263cb6c0f2ba7b49d32f08
Nicholas McKenzie <nicholas.a.mckenzie@gmail.com>
2019-11-15 15:26:13 -0500

    Implemented setInterval between movement queue run execution.

--- scripts/script.js
+++ scripts/script.js
@@ -11,31 +11,96 @@ let caterpillarTrackingArray = ["91"];
 
 caterpillar.drawFunction = () => {
     $(".item").removeClass("draw");
+
     $(`.item${caterpillarTrackingArray[0]}`).toggleClass("draw");
 };
 
 caterpillar.drawFunction();
 // -------------------------------------------------------
 
+// // Hello is alerted repeatedly after every 3 seconds
+// let timerId = setInterval(() => alert('Hello'), 3000);
+
+// // Clear intervals after 6 sec with the timer id 
+// setTimeout(() => { clearInterval(timerId); alert('Bye'); }, 6000);
+
+// json.objects.forEach(function (obj, index, collection) {
+//     setTimeout(function () {
+//         console.log('foobar');
+//         self.insertDesignJsonObject(obj, index);
+//     }, index * 5000);
+// });
+
 
 
 // -----------------------------------------------------
-// creating move button functionality
+// creating button functionality
 
 $(".moveButton").mousedown(function () {
     const buttonType = $(this).attr("id");
     // caterpillar.buttonAction(buttonType);
     if (buttonType === "runButton") {
         // caterpillar.drawFunction();
-        movementQueueArray.forEach(function(movement) {
-            caterpillar.buttonAction(movement)
+        // movementQueueArray.forEach(function(movement) {
+        //     caterpillar.buttonAction(movement)
+        // });
+        movementQueueArray.forEach(function(movement, index) {
+            setTimeout(function() {
+                // caterpillar.buttonAction(movement);
+                console.log(movement);
+                caterpillar.buttonAction(movement);
+            }, index * 2000);
+            // clearInterval(intervalID);
+
         });
+
+        // for ( let i = 0; i < movementQueueArray.length; i++) {
+        //     // setInterval(function() {
+        //     //     console.log(movementQueueArray[i]);
+        //     // }, 1000);
+        //     console.log("outside timout", movementQueueArray[i]);
+        //     setTimeout(function() {
+        //         console.log("inside timeout", movementQueueArray[i]);
+        //     }, 5000);
+
+
+        // };
+
+        // for (let i = 0; i < movementQueueArray.length; i++) {
+        //     // (function (i) {
+        //     //     // setInterval(function () {
+        //     //     //     // movementQueueArray[i] += 1;
+        //     //     //     console.log(movementQueueArray[i]);
+        //     //     // }, 3000)
+                
+        //     // })(i);
+        //     console.log(movementQueueArray[i]);
+
+        // };
+
+        // var index = 0;
+        // var interval = setInterval(function () {
+        //     console.log(movementQueueArray[index++]);
+        //     if (index == movementQueueArray.length) {
+        //         clearInterval(interval);
+        //     }
+        // }, 2000);
+
+
+
         movementQueueArray.length = 0;
+
+    } else if (buttonType === "resetButton") {
+        movementQueueArray.length = 0;
+        caterpillarTrackingArray.length = 0;
+        caterpillarTrackingArray.push(91);
+        caterpillar.drawFunction();
     } else {
         movementQueueArray.push(buttonType);
         console.log(movementQueueArray);
     }
 });
+
 
 caterpillar.buttonAction = (buttonType) => {
     if (buttonType === "upButton") {
