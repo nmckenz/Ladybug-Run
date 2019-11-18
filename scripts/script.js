556b3662d49652f491aa9168374f8fb99026bf24
Nicholas McKenzie <nicholas.a.mckenzie@gmail.com>
2019-11-17 23:44:35 -0500

    CSS styling.

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,27 @@
 // NAMESPACE==============================================
-const caterpillar = {}
+const caterpillar = {};
 // =======================================================
 
+
+
+// GLOBAL VARIABLES--------------------------------------------
 let runTracker = 0;
 let gameTracker = 0;
 let winTracker = 0;
+let queueTracker = 0;
 let finalPositionTracker = 0;
 let $leafPosition;
 let $caterpillarPosition;
-// -------------------------------------------------------
-// toggle class for drawing caterpillar and leaf
+let movementQueueArray = [];
+// ---------------------------------------------------------
 
+
+
+// BUG AND LEAF --------------------------------------------
 let caterpillarTrackingArray = [91];
-const caterpillarDrawing = `<div class="body">
+const caterpillarDrawing = `<div class="bugBody">
 <div class="eyes left"></div>
 <div class="eyes right"></div>
-
 <div class="spot spot1"></div>
 <div class="spot spot2"></div>
 <div class="spot spot3"></div>
@@ -24,54 +30,52 @@ const caterpillarDrawing = `<div class="body">
 <div class="spot spot6"></div>
 </div>`;
 
-// const leafTrackingArray = [(Math.floor(Math.random() * 100) + 1).toString()];
 let leafTrackingArray = [];
 // console.log("leaf tracking", leafTrackingArray);
-
+const leafDrawing = `<img src="./assets/leaf.png" alt="shady spot for our scorched ladybug to rest" class="leafImage">`;
 
 caterpillar.leafRandomize = () => {
     let leafPos = (Math.floor(Math.random() * 100) + 1);
     if (leafPos === 91) {
         caterpillar.leafRandomize()
     } else {
-        // return leafPos.toString();
-        // leafTrackingArray[0] = leafPos.toString();
         leafTrackingArray[0] = leafPos;
         $leafPosition = leafTrackingArray[0];
-
-
-    }
+    };
 };
-
-// let x = caterpillar.leafRandomize();
-// console.log("x", x)
-// leafTrackingArray[0] = x;
 
 caterpillar.leafRandomize();
 // console.log("leaf tracking after func", leafTrackingArray);
 
+caterpillar.finalPositionCalc = () => {
+    let finalPosition = 91;
+    movementQueueArray.forEach(function (movement) {
+        if (movement === "upButton" && finalPosition > 10) {
+            finalPosition -= 10;
+        } else if (movement === "downButton" && finalPosition <= 90) {
+            finalPosition += 10;
+        } else if (movement === "leftButton" && finalPosition % 10 != 1) {
+            finalPosition -= 1;
+        } else if (finalPosition % 10 != 0) {
+            finalPosition += 1;
+        };
+    });
+    return finalPosition;
+};
+// ---------------------------------------------------------
 
-const leafDrawing = `<img src="./assets/leaf.png" alt="shady spot for our scorched ladybug to rest" class="leafImage">`;
 
+
+// DRAWING----------------------------------------------------
 caterpillar.drawFunction = (imgPosition, imgType) => {
-    // $(".item").removeClass("draw");
-    
-    // $(`.item${caterpillarTrackingArray[0]}`).toggleClass("draw");
-    
-    // $(`.item${caterpillarTrackingArray[0]}`).append(caterpillarDrawing);
-    
-    // imgPosition.append(imgType);
-    // console.log("img pos", imgPosition);
-    // console.log("img type", imgType);
-    // $(`.item${imgPosition}`).append(imgType);
+    // if ($caterpillarPosition !== $leafPosition) {
+    //     $(`.item${imgPosition}`).append(imgType);
+    // } else {
+    //     // $(`.item${imgPosition}`).html("");
+    //     // $(`.item${imgPosition}`).append(leafDrawing);
+    // };
+    $(`.item${imgPosition}`).append(imgType);
 
-    if ($caterpillarPosition !== $leafPosition) {
-        $(`.item${imgPosition}`).append(imgType);
-    } else {
-        $(`.item${imgPosition}`).html("");
-        $(`.item${imgPosition}`).append(leafDrawing);
-    };
-    
 };
 
 $caterpillarPosition = caterpillarTrackingArray[0];
@@ -81,13 +85,25 @@ $leafPosition = leafTrackingArray[0];
 
 caterpillar.drawFunction($caterpillarPosition, caterpillarDrawing);
 caterpillar.drawFunction($leafPosition, leafDrawing);
+// ------------------------------------------------------
 
 
 
-
-
-// caterpillar.drawBug
-// -------------------------------------------------------
+// GAME WIN AND RESET-----------------------------------------
+caterpillar.gameWinCheck = () => {
+    if ($caterpillarPosition === $leafPosition && finalPositionTracker === $leafPosition) {
+        winTracker = 1;
+        if (confirm("The ladybug has it made in the shade! Do you wish to play again?")) {
+            // console.log("user plays again")
+            // caterpillar.leafRandomize();
+            // console.log("leaf pos after win", $leafPosition);
+            caterpillar.gameReset();
+        } else {
+            // console.log("user done");
+            alert("Thanks for playing!");
+        }
+    };
+};
 
 caterpillar.gameReset = () => {
     // console.log("game reset func call");
@@ -112,54 +128,24 @@ caterpillar.bugReset = () => {
     // console.log("bug reset func call");
     $(`.item${caterpillarTrackingArray[0]}`).html("");
     $(".movementQueue").empty();
+    $(".movementQueue").append("<h4>queue:</h4>");
     movementQueueArray.length = 0;
     caterpillarTrackingArray[0] = 91;
     $caterpillarPosition = caterpillarTrackingArray[0];
     caterpillar.drawFunction($caterpillarPosition, caterpillarDrawing);
-}
+};
 
 caterpillar.leafReset = () => {
     // console.log("leaf reset func call");
     caterpillar.leafRandomize();
     caterpillar.drawFunction($leafPosition, leafDrawing);
-}
-
-caterpillar.gameWinCheck = () => {
-    if (finalPositionTracker === $leafPosition) {
-        winTracker = 1;
-        if (confirm("The ladybug has it made in the shade! Do you wish to play again?")) {
-            // console.log("user plays again")
-            // caterpillar.leafRandomize();
-            // console.log("leaf pos after win", $leafPosition);
-            caterpillar.gameReset();
-        } else {
-            // console.log("user done");
-            alert("Thanks for playing!");
-        }
-    };
-}
-
-
-caterpillar.finalPositionCalc = () => {
-    let finalPosition = 91;
-    movementQueueArray.forEach(function (movement) {
-        if (movement === "upButton" && finalPosition > 10) {
-            finalPosition -= 10;
-        } else if (movement === "downButton" && finalPosition <= 90) {
-            finalPosition += 10;
-        } else if (movement === "leftButton" && finalPosition % 10 != 1) {
-            finalPosition -= 1;
-        } else if (finalPosition % 10 != 0) {
-            finalPosition += 1;
-        };
-    });
-    return finalPosition;
 };
+// ---------------------------------------------------------
 
 
-// -----------------------------------------------------
-// creating button functionality
 
+
+// BUTTON FUNCTIONALITY-----------------------------------------
 $(".button").mousedown(function () {
     const buttonType = $(this).attr("id");
     // caterpillar.buttonAction(buttonType);
@@ -181,10 +167,11 @@ $(".button").mousedown(function () {
                 }, index * 1500);
             });
             movementQueueArray.length = 0;
+            queueTracker = 1;
             // console.log("queue array after run", movementQueueArray);
 
             // console.log("caterpillar pos after run", $caterpillarPosition);
-        }
+        };
 
         // $(".gridButtons").toggleClass("hidden");
 
@@ -198,13 +185,13 @@ $(".button").mousedown(function () {
         // console.log("queue array after run", movementQueueArray);
         // console.log("caterpillar pos after run", $caterpillarPosition);
         // $(".gridButtons").toggleClass("inlineBlock");
-
-
     } else if (buttonType === "resetButton") {
         if (movementQueueArray[0] === undefined && runTracker === 0) {
             alert("The poor ladybug is confused, she needs to know where to go first! Please give her some directions.")
-        } else if (movementQueueArray.length != 0) {
+        } else if (movementQueueArray.length !== 0 && runTracker === 1 && gameTracker === 1) {
             alert("I know she is ponderous and slow, but let her get where's going first or she'll get lost and confused!")
+        } else if (movementQueueArray.length !== 0 && runTracker === 0) {
+            caterpillar.bugReset();
         } else {
             // $(`.item${caterpillarTrackingArray[0]}`).html("");
             // $(".movementQueue").empty();
@@ -218,19 +205,27 @@ $(".button").mousedown(function () {
 
             // caterpillar.gameReset();
 
+            // console.log("reset button press")
+
+
             if (winTracker === 0) {
                 caterpillar.bugReset();
                 runTracker = 0;
                 gameTracker = 0;
+                queueTracker = 0;
             } else {
                 caterpillar.gameReset();
             }
 
-        }
+        };
     } else if (buttonType === "closeInstructions") {
-        $(".gameInstructions").toggleClass("hidden");
+        $(".instructionsWrapper").toggleClass("hidden");
+        // $(".grid").toggleClass("hidden");
+        // $(".gameControls").toggleClass("hidden")
     } else if (buttonType === "openInstructions") {
-        $(".gameInstructions").toggleClass("hidden");
+        $(".instructionsWrapper").toggleClass("hidden");
+        // $(".grid").toggleClass("hidden");
+        // $(".gameControls").toggleClass("hidden")
     } else {
         if (gameTracker === 1 && winTracker === 0) {
             alert("Ladybug is all out of moves. Please hit reset button to play again.");
@@ -241,10 +236,24 @@ $(".button").mousedown(function () {
             $(".movementQueue").append(`<p>${$(this).attr("id").replace("Button", "")}`);
             runTracker = 1;
             // console.log("run tracker", runTracker);
-        }
-    }
+            // $(".movementQueue").append(`<p>${icons["buttonType"]}</p>`)
+
+            }
+        };
+    };
 });
 
+// const icons = {
+//     upButton: `<i class="fas fa-arrow-up"></i>`,
+//     downButton: `<i class="fas fa-arrow-down"></i>`,
+//     leftButton: `<i class="fas fa-arrow-left"></i>`,
+//     rightButton: `<i class="fas fa-arrow-right"></i>`
+// };
+
+// {/* <i class="fas fa-arrow-up"></i>
+// <i class="fas fa-arrow-down"></i>
+// <i class="fas fa-arrow-left"></i>
+// <i class="fas fa-arrow-right"></i> */}
 
 caterpillar.buttonAction = (buttonType) => {
     if (buttonType === "upButton") {
@@ -253,9 +262,11 @@ caterpillar.buttonAction = (buttonType) => {
             const newPosition = $caterpillarPosition - 10;
             // console.log("new position", newPosition);
 
-            $(`.item${caterpillarTrackingArray[0]}`).html("");
+            // $(`.item${caterpillarTrackingArray[0]}`).html("");
+
             // $(`.item${$caterpillarPosition}`).html("");
             // console.log("up button calc", $caterpillarPosition);
+            $(".bugBody").remove();
             
 
             caterpillarTrackingArray.splice(0, 1, newPosition);
@@ -279,7 +290,8 @@ caterpillar.buttonAction = (buttonType) => {
 
 
             // }
-            console.log("final position track before wincheck", finalPositionTracker);
+
+            // console.log("final position track before wincheck", finalPositionTracker);
 
             caterpillar.gameWinCheck();
         };
@@ -288,7 +300,9 @@ caterpillar.buttonAction = (buttonType) => {
             // const newPosition = caterpillarTrackingArray[0] + 10;
             const newPosition = $caterpillarPosition + 10;
 
-            $(`.item${caterpillarTrackingArray[0]}`).html("");
+            // $(`.item${caterpillarTrackingArray[0]}`).html("");
+            $(".bugBody").remove();
+
 
             caterpillarTrackingArray.splice(0, 1, newPosition);
             // $caterpillarPosition = caterpillarTrackingArray.splice(0, 1, newPosition);
@@ -304,7 +318,9 @@ caterpillar.buttonAction = (buttonType) => {
             // const newPosition = caterpillarTrackingArray[0] - 1;
             const newPosition = $caterpillarPosition - 1;
 
-            $(`.item${caterpillarTrackingArray[0]}`).html("");
+            // $(`.item${caterpillarTrackingArray[0]}`).html("");
+            $(".bugBody").remove();
+
 
             caterpillarTrackingArray.splice(0, 1, newPosition);
             // $caterpillarPosition = caterpillarTrackingArray.splice(0, 1, newPosition);
@@ -321,7 +337,9 @@ caterpillar.buttonAction = (buttonType) => {
             const newPosition = $caterpillarPosition + 1;
 
 
-            $(`.item${caterpillarTrackingArray[0]}`).html("");
+            // $(`.item${caterpillarTrackingArray[0]}`).html("");
+            $(".bugBody").remove();
+
 
             caterpillarTrackingArray.splice(0, 1, newPosition);
             // $caterpillarPosition = caterpillarTrackingArray.splice(0, 1, newPosition);
@@ -332,9 +350,13 @@ caterpillar.buttonAction = (buttonType) => {
 
             caterpillar.gameWinCheck();
         };
-    }
+    };
     // console.log("bug track before new call", $caterpillarPosition)
-    caterpillar.drawFunction($caterpillarPosition, caterpillarDrawing);
+    // caterpillar.drawFunction($caterpillarPosition, caterpillarDrawing);
+
+    if ($caterpillarPosition !== $leafPosition) {
+        caterpillar.drawFunction($caterpillarPosition, caterpillarDrawing);
+    };
 };
 // ------------------------------------------------------
 
@@ -342,7 +364,7 @@ caterpillar.buttonAction = (buttonType) => {
 
 // --------------------------------------------------------
 // array for tracking movement selections
-let movementQueueArray = [];
+// let movementQueueArray = [];
 // console.log("queue array init", movementQueueArray);
 // -------------------------------------------------------
 
@@ -350,6 +372,8 @@ let movementQueueArray = [];
 
 // INIT===================================================
 caterpillar.init = () => {
+    // $(".grid").toggleClass("hidden");
+    // $(".gameControls").toggleClass("hidden");
     // console.log("hello world");
 };
 // =======================================================
